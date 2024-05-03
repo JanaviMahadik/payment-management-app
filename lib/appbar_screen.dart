4158940c54a2e917bfb465ac0da01780bce2618f
JanaviMahadik <janavimahadik06@gmail.com>
2024-05-04 03:37:37 +0530

    added the search bar text and changed the photo of send again users

--- lib/appbar_screen.dart
+++ lib/appbar_screen.dart
@@ -101,6 +101,19 @@ class _AppbarScreenState extends State<AppbarScreen> implements AppbarView {
             ),
           ),
           SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                'Send Again',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
             child: Container(
               height: 100.0, // Adjust the height as needed
               child: ListView.builder(
@@ -112,8 +125,7 @@ class _AppbarScreenState extends State<AppbarScreen> implements AppbarView {
                     child: Column(
                       children: [
                         CircleAvatar(
-                          backgroundImage: NetworkImage('https://th.bing.com/th?id=OIP.Cyh8Uho74DcMA4MTnMUjbAHaLC&w=204&h=305&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2'),
-                          radius: 30.0,
+                          backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.E95e-X6l6dr3sXlS2Gc7lwHaHa?w=201&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7'),
                         ),
                         SizedBox(height: 4.0),
                         Text(
